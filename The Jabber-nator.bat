8ec5547c181f41c40f3f4ab44b23a45b8097f301
Will Brandow <wsbrandow@gmail.com>
2018-09-13 14:22:56 -0700

    first commit

---
+++ The Jabber-nator.bat
@@ -0,0 +1,11 @@
+pause
+taskkill /f /IM CiscoJabber.exe
+pause
+rmdir /s /q "C:\Users\%username%\AppData\Local\Cisco\Unified Communications\"
+rmdir /s /q "C:\Users\%username%\AppData\Roaming\Cisco\Unified Communications\"
+pause
+
+
+
+
+
